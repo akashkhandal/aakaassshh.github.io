1f21b67b7ac7fa522b5b2e307a415859336d04b6
Akash Khandal <126162733+aakaassshh@users.noreply.github.com>
2023-05-21 17:54:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+$(".twitter").hover(function () {$(".social-container-wrap").toggleClass("color-twitter")});
+$(".facebook").hover(function () {$(".social-container-wrap").toggleClass("color-facebook")});
+$(".googleplus").hover(function () {$(".social-container-wrap").toggleClass("color-googleplus")});
+$(".pinterest").hover(function () {$(".social-container-wrap").toggleClass("color-pinterest")});
+$(".dribbble").hover(function () {$(".social-container-wrap").toggleClass("color-dribbble")});
+$(".instagram").hover(function () { $(".social-container-wrap").toggleClass("color-instagram") });
+$(".codepen").hover(function () { $(".social-container-wrap").toggleClass("color-codepen") });
+$(".envelope").hover(function () { $(".social-container-wrap").toggleClass("color-envelope") });
+$(".linkedin").hover(function () { $(".social-container-wrap").toggleClass("color-linkedin") });
